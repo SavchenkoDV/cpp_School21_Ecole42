5844514337f8880ffe22946b0031c48cd86d3c30
buthor <dvs.rocket@gmail.com>
2021-12-19 17:45:48 +0300

    module02 ex00 done

--- module_02/ex00/Fixed.cpp
+++ module_02/ex00/Fixed.cpp
@@ -1,23 +1,35 @@
 #include "Fixed.hpp"
 
+
+
 Fixed::Fixed() {
 	value = 0;
-	std::cout << "Constructor to address: " << this << std::endl;
+	std::cout << "Default constructor to address: " << this << " called" <<  std::endl;
 }
 
 Fixed::~Fixed() {
-	std::cout << "Destructor to address: " << this << std::endl;
+	std::cout << "Destructor to address: " << this << " called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &copy_constructor) {
 	this->value = copy_constructor.value;
-	std::cout << "Copy of constructor on address: " << this << std::endl;
+	std::cout << "Copy of constructor on address: " << this << " called" << std::endl;
 }
 
 Fixed &Fixed::operator=(const Fixed &overload) {
 	this->value = overload.value;
-	std::cout << "Assignment operator Address: " << this << std::endl;
+	std::cout << "Assignment operator called" << std::endl;
 	return *this;
+}
+
+void Fixed::setRawBits(int const raw) {
+	std::cout << "getRawBits member function called" << std::endl;
+	this->value = raw;
+}
+
+int Fixed::getRawBits() const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return value;
 }
 
 
--- module_02/ex00/main.cpp
+++ module_02/ex00/main.cpp
@@ -3,13 +3,13 @@
 int main( void ) {
 
 	Fixed a;
-	Fixed b( a );
+	Fixed b ( a );
 	Fixed c;
 
 	c = b;
 
-//	std::cout << a.getRawBits() << std::endl;
-//	std::cout << b.getRawBits() << std::endl;
-//	std::cout << c.getRawBits() << std::endl;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
 	return 0;
 }
